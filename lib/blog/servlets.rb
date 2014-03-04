cc6bb2047a657c0b9341b6fdd2e06446051acb24
Erick C <ecastillo@pernix-solutions.com>
2014-03-04 09:05:23 -0600

    Tests as servlets changed to just tests

--- lib/blog/servlets.rb
+++ lib/blog/servlets.rb
@@ -1,3 +1,2 @@
 require_relative 'servlets/index'
 require_relative 'servlets/post_form'
-require_relative 'servlets/post_manager'
--- spec/blog/post_form_spec.rb
+++ spec/blog/post_form_spec.rb
@@ -1,77 +1,60 @@
-require 'webrick'
 require 'net/http'
 require 'rtest'
 require_relative 'constants'
+require_relative 'blog'
 
-include WEBrick
+include RTest::MainTest
 
-module Blog
-  class PostFormTest < HTTPServlet::AbstractServlet
-    include RTest::MainTest
+FILE = File.expand_path(File.join(File.dirname(__FILE__), '../test_index.xml'))
+ORIGINAL_FILE = File.read(FILE)
+URI_INDEX_ADDRESS = 'http://localhost:8000/'
+URI_POST_ADDRESS = 'http://localhost:8000/post'
 
-    FILE = File.expand_path(File.join(File.dirname(__FILE__), '../test_index.xml'))
-    ORIGINAL_FILE = File.read(FILE)
-    URI_INDEX_ADDRESS = 'http://localhost:8000/'
-    URI_POST_ADDRESS = 'http://localhost:8000/post'
+def reset_file
+  File.open(FILE, "w") { |data| data << ORIGINAL_FILE}
+end
 
-    def self.reset_file
-      File.open(FILE, "w") { |data| data << ORIGINAL_FILE}
-    end
+server = Blog::Server.new.server
+Thread.new() { server.start }
+Blog::XMLManager.set_xml(FILE)
 
-    def do_GET(req, res)
-      run
-    end
-
-    def do_POST(req, res)
-      run
-    end
-
-    private
-    def run
-      XMLManager.set_xml(FILE)
-      declare_tests
-      run_tests
-    end
-
-
-    def declare_tests
-      the Blog::Index, " no post is passed" do
-        has_to "ask for new information" do
-          uri = URI(URI_POST_ADDRESS)
-          res = Net::HTTP.get_response(uri)
-          include_body = res.body.include?('<textarea id="content" name="content">Content here...</textarea>')
-          include_name = res.body.include?('<input id="name" type="text" name="name" value=""/>')
-          include_id = res.body.include?('<input type="hidden" name="id" value=""/>')
-          correct_title = res.body.include?('<h2>New Post</h2>')
-          expect(include_name).to equal(true)
-          expect(include_body).to equal(true)
-          expect(include_id).to equal(true)
-          expect(correct_title).to equal(true)
-          IndexTest.reset_file
-        end
-      end
-
-      the Blog::Index, " post is passed" do
-        has_to "display the post information" do
-          uri = URI(URI_INDEX_ADDRESS)
-          params = { name: A_NAME, content: A_CONTENT, id: NO_ID, action: SAVE }
-          uri.query = URI.encode_www_form(params)
-          res = Net::HTTP.get_response(uri)
-          uri = URI(URI_POST_ADDRESS)
-          params = { name: A_NAME, content: A_CONTENT, id: NEW_POST_ID }
-          uri.query = URI.encode_www_form(params)
-          res = Net::HTTP.get_response(uri)
-          include_body = res.body.include?('<textarea id="content" name="content">' + "#{A_CONTENT}" + '</textarea>')
-          include_name = res.body.include?('<input id="name" type="text" name="name" value="' + "#{A_NAME}" + '"/>')
-          include_id = res.body.include?('<input type="hidden" name="id" value="' + "#{NEW_POST_ID}" + '"/>')
-          correct_title = res.body.include?('<h2>Edit Post</h2>')
-          expect(include_name).to equal(true)
-          expect(include_body).to equal(true)
-          expect(include_id).to equal(true)
-          expect(correct_title).to equal(true)
-          IndexTest.reset_file
-        end
-      end
-    end
+the Blog::Index, " no post is passed" do
+  has_to "ask for new information" do
+    uri = URI(URI_POST_ADDRESS)
+    res = Net::HTTP.get_response(uri)
+    include_body = res.body.include?('<textarea id="content" name="content">Content here...</textarea>')
+    include_name = res.body.include?('<input id="name" type="text" name="name" value=""/>')
+    include_id = res.body.include?('<input type="hidden" name="id" value=""/>')
+    correct_title = res.body.include?('<h2>New Post</h2>')
+    expect(include_name).to equal(true)
+    expect(include_body).to equal(true)
+    expect(include_id).to equal(true)
+    expect(correct_title).to equal(true)
+    reset_file
   end
 end
+
+the Blog::Index, " post is passed" do
+  has_to "display the post information" do
+    uri = URI(URI_INDEX_ADDRESS)
+    params = { name: A_NAME, content: A_CONTENT, id: NO_ID, action: SAVE }
+    uri.query = URI.encode_www_form(params)
+    res = Net::HTTP.get_response(uri)
+    uri = URI(URI_POST_ADDRESS)
+    params = { name: A_NAME, content: A_CONTENT, id: NEW_POST_ID }
+    uri.query = URI.encode_www_form(params)
+    res = Net::HTTP.get_response(uri)
+    include_body = res.body.include?('<textarea id="content" name="content">' + "#{A_CONTENT}" + '</textarea>')
+    include_name = res.body.include?('<input id="name" type="text" name="name" value="' + "#{A_NAME}" + '"/>')
+    include_id = res.body.include?('<input type="hidden" name="id" value="' + "#{NEW_POST_ID}" + '"/>')
+    correct_title = res.body.include?('<h2>Edit Post</h2>')
+    expect(include_name).to equal(true)
+    expect(include_body).to equal(true)
+    expect(include_id).to equal(true)
+    expect(correct_title).to equal(true)
+    reset_file
+  end
+end
+
+run_tests
+server.shutdown
